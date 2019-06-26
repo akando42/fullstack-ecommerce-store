02172973cc670d2716cd65f48d56b8ff1b2de8cb
Troy Do <do@infinitylab.world>
2019-06-26 02:40:28 -0500

    Create MYSQL db and Connect to it via NOdejS

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const os = require('os');
+const mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "remotemysql.com",
+  user: "PqIDRs1G70",
+  password: "BDrBcLor07"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 const app = express();
 
@@ -7,3 +18,5 @@ app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
+
