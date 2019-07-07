2eee62b155d612eb2af73d130666e92133e5ae8b
Troy Do <do@infinitylab.world>
2019-07-07 17:59:11 -0500

    factoring Subscription Form

--- src/client/components/SubscriptionForm/SubscriptionForm.actions.js
+++ src/client/components/SubscriptionForm/SubscriptionForm.actions.js
@@ -1,3 +1,3 @@
-import * as types from './ActionTypes'
+import * as types from '../../constants/ActionTypes'
 export const addSubscriber = email => ({ type: types.ADD_SUBSCRIBER, email })
 export const verifySubscriber = email => ({ type: types.VERIFY_SUBSCRIBER, email })
